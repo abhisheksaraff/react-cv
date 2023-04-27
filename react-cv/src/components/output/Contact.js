30e7c31548c10a5e26f8a388f598b8064d73edfd
a6hi <saraffabhishek@outlook.com>
2023-04-26 23:00:14 -0400

    made profileLink in Contact redirect to user's website

--- react-cv/src/components/output/Contact.js
+++ react-cv/src/components/output/Contact.js
@@ -9,16 +9,29 @@ class Contact extends Component {
     return (
       <div>
         {this.props.contact.phone}
-        
+
         {/* checks if either email or phone contain something, then renders a '|' after phone number*/}
-        {((this.props.contact.email !== undefined && this.props.contact.email !== "") && (this.props.contact.phone !== undefined && this.props.contact.phone !== "")) && <> | </>}
-        
+        {this.props.contact.email !== undefined &&
+          this.props.contact.email !== "" &&
+          this.props.contact.phone !== undefined &&
+          this.props.contact.phone !== "" && <> | </>}
+
         {this.props.contact.email}
-        
+
         {/* checks if either email or phone or onlineProfilne contain something, then renders a '|' before onlineProfile */}
-        {((this.props.contact.phone !== undefined && this.props.contact.phone !== "") && (this.props.contact.onlineProfile !== undefined && this.props.contact.onlineProfile !== "") || (this.props.contact.email !== undefined && this.props.contact.email !== "") && (this.props.contact.onlineProfile !== undefined && this.props.contact.onlineProfile !== "")) && <> | </>}
-        
-        {this.props.contact.onlineProfile}
+        {((this.props.contact.phone !== undefined &&
+          this.props.contact.phone !== "" &&
+          this.props.contact.onlineProfile !== undefined &&
+          this.props.contact.onlineProfile !== "") ||
+          (this.props.contact.email !== undefined &&
+            this.props.contact.email !== "" &&
+            this.props.contact.onlineProfile !== undefined &&
+            this.props.contact.onlineProfile !== "")) && <> | </>}
+
+        <a rel="noopener noreferrer" target="_blank" href={`https:\\${this.props.contact.onlineProfile}`}>
+          {this.props.contact.onlineProfile}
+        </a>
+
       </div>
     );
   }
