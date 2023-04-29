389d74b976a42a07c70bb42ac422ec97879420be
a6hi <saraffabhishek@outlook.com>
2023-04-28 23:20:20 -0400

    fixed error where comma would appear even if there is no state entered

--- react-cv/src/components/output/Education.js
+++ react-cv/src/components/output/Education.js
@@ -15,7 +15,10 @@ class Education extends Component {
         <div>
           {this.props.education.description}
           {this.props.education.city}
-          {(this.props.education.city !== undefined && this.props.education.city !== "") && <>,</>}
+
+          {/* renders , if city and state are not undefined/empty */}
+          {(this.props.education.city !== undefined && this.props.education.city !== "" && this.props.education.state !== undefined && this.props.education.state !== "" ) && <>, </>}
+          
           {this.props.education.state}
         </div>
       </div>
